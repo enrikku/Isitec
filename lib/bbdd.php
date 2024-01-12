839359467f6ba9a141b3d0191a5324e0bcca90ba
enrikku <enric.cancru@gmail.com>
2024-01-12 10:52:47 +0100

    Codigo del Login y SignIn hecho

--- lib/bbdd.php
+++ lib/bbdd.php
@@ -23,24 +23,86 @@ function compruebaUsuario($user, $pass, $opcion)
 
     // Si es 1 es email, si es 2 es username
     if ($opcion == 1) {
-        $sql = "SELECT * FROM users WHERE mail = :user AND passHash = :pass";
+        $sql = "SELECT * FROM users WHERE mail = :user";
     } else {
-        $sql = "SELECT * FROM users WHERE username = :user AND passHash = :pass";
+        $sql = "SELECT * FROM users WHERE username = :user";
     }
 
     $logged = false;
 
     try {
         $resultat = $db->prepare($sql);
-        $resultat->execute([":user" => $user, ":pass" => $pass]);
+        $resultat->execute([":user" => $user]);
         $row = $resultat->fetch(PDO::FETCH_ASSOC);
 
-        if ($row) {
+        if ($row && password_verify($pass, $row['passHash'])) {
             $logged = true;
         }
     } catch (PDOException $e) {
         echo "Error: " . $e->getMessage();
     } finally {
         return $logged;
+    }
+}
+
+
+function sigin($user, $pass, $mail, $nombre, $apellidos){
+    $dbo = conexion();
+    $signed = false;
+
+    $sql = "INSERT INTO users (username, passHash, mail, userFirstName, userLastName, creationDate) 
+                        VALUES (:user, :pass, :mail, :nombre, :apellidos, NOW())";
+
+    if(!existeMail($mail))
+    {
+        try{
+
+            $pass = password_hash($pass, PASSWORD_DEFAULT);
+
+            $resultat = $dbo->prepare($sql);
+            $resultat->execute([":user" => $user, 
+                                ":pass" => $pass, 
+                                ":mail" => $mail, 
+                                ":nombre" => $nombre, 
+                                ":apellidos" => $apellidos
+                            ]);
+            $row = $resultat->fetch(PDO::FETCH_ASSOC);
+    
+            if ($row) {
+                $signed = true;
+            }
+        }
+        catch (PDOException $e) {
+            echo "Error: " . $e->getMessage();
+        } finally {
+            return $signed;
+        }
+    }
+    else{
+        $signed = false;
+        return $signed;
+    }
+
+    
+}
+
+function existeMail($mail){
+    $db = conexion();
+    $existe = false;
+
+    $sql = "SELECT * FROM users WHERE mail = :mail";
+
+    try{
+        $resultat = $db->prepare($sql);
+        $resultat->execute([":mail" => $mail]);
+        $row = $resultat->fetch(PDO::FETCH_ASSOC);
+
+        if ($row) {
+            $existe = true;
+        }
+    }catch (PDOException $e) {
+        echo "Error: " . $e->getMessage();
+    }finally {
+        return $existe;
     }
 }
--- public/authentication/sigin.php
+++ public/authentication/sigin.php
@@ -2,11 +2,55 @@
     require __DIR__ . '\..\..\lib\bbdd.php';
 
     $db = conexion();
+    $singed = false;
+    $validEmail = false;
+    $equalPass = false;
+    $errMsg = "";
 
-    if(count($_POST) == 2){
+    if(count($_POST) == 6){
+        $user   = isset($_POST["user"]) ? filter_input(INPUT_POST,'user',FILTER_SANITIZE_STRING) : '';
+        $nombre = isset($_POST["nombre"]) ? filter_input(INPUT_POST,'nombre',FILTER_SANITIZE_STRING) : '';
+        $apellidos = isset($_POST["apellidos"]) ? filter_input(INPUT_POST,'apellidos',FILTER_SANITIZE_STRING) : '';
+
+        $pass   = isset($_POST["pass"]) ? filter_input(INPUT_POST,'pass',FILTER_SANITIZE_STRING) : '';
+        $pass2   = isset($_POST["pass2"]) ? filter_input(INPUT_POST,'pass2',FILTER_SANITIZE_STRING) : '';
         
-    }
+        if($pass == $pass2){
+            $equalPass = true;
+        }else{
+            $errMsg = "Las contraseñas no coinciden";
+            $equalPass = false;
+        }
+     
+        $mail = isset($_POST["mail"]) ? $_POST["mail"] : '';
+        
+        $regexMail = '/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/';
 
+        if (preg_match($regexMail, $mail)) {
+            $validEmail = true;
+        } else {
+            $errMsg = "El correo electrónico no es válido";
+            $validEmail = false;
+        }   
+        
+        if($validEmail == true && $equalPass == true){
+            if(existeMail($mail)){
+                $errMsg = "El correo electrónico ya existe";
+            }
+            
+            $signed = sigin($user, $pass, $mail, $nombre, $apellidos);
+        }else{
+            $signed = false;
+        }
+
+
+        if($singed == true){
+           header("Location: ./login.php");
+        }
+        else{
+            
+        }
+    }
 ?>
 
 <!DOCTYPE html>
@@ -20,34 +64,30 @@
 <body>
 
 
-    <form action="login.php" method="post">
-
+    <form action="sigin.php" method="post">
         <label for="user">Username
-            <input type="text" name="user" id="user" placeholder="username">
+            <input type="text" name="user" id="user" required placeholder="username">
         </label>
-
-        <label for="mail">Email
-            <input type="text" name="mail" id="mail" placeholder="email">
-        </label>
-
+        
+        <label for="mail">Email: </label>
+            <input type="email" name="mail" id="mail" required placeholder="email">
+        
 
         <label for="nombre">Primer nombre
             <input type="text" name="nombre" id="nombre" placeholder="Prmer nombre">
         </label>
-
         <label for="apellidos">Apellidos</label>
             <input type="text" name="apellidos" id="apellidos" placeholder="Apellidos">
         </label>
-
         <label for="pass">Contraseña:
             <input type="password" name="pass" id="pass" required placeholder="Ej: 9807@abstract">
         </label>
-
         <label for="pass2">Repite la contraseña:
             <input type="text" name="pass2" id="pass2" required placeholder="Repite la contraseña">
         </label>
 
-        <input type="submit" value="login">
+        <p><?php echo $errMsg ?></p>
+        <input type="submit" value="sigin">
     </form>
 </body>
 </html>
